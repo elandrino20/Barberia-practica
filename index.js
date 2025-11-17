c7e0f06f0472f6bbed13f362fe7b0694b4f7254b
elandrino20 <alejandro01384596@gmail.com>
2025-11-17 16:47:50 -0500

    Puerto configurado para Render

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('./database');
 
 const app = express();
 
+// Puerto para Render
 app.set("port", process.env.PORT || 4000);
 
 // Middleware
@@ -14,9 +15,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Rutas
-
 app.use('/api/citas', require('./src/routers/cita.route'));
 
+// ------------------------
+// 🔥 Servidor (IMPORTANTE)
+// ------------------------
+app.listen(app.get("port"), () => {
+  console.log("Servidor corriendo en el puerto:", app.get("port"));
+});
 
 
 
